bc752c37c6022632aa57e8f34a2bb81ba46d31f8
Sreekanth <2010040081@klh.edu.in>
2023-09-09 13:20:50 +0530

    Audio_to_text

--- ATT.py
+++ ATT.py
@@ -1,48 +1,26 @@
-## audio to text is workdin, need to work on the accuracy
 
-import os
+import moviepy.editor as mp
 import speech_recognition as sr
 
-# Function to transcribe audio and save the transcription to a text file
-def transcribe_audio(audio_file_path):
-    # Initialize the recognizer
-    recognizer = sr.Recognizer()
+# Load the video
+video = mp.VideoFileClip("C://Users//srika//OneDrive//Desktop//Sree//Hello WD//Hackthon//test_audio.mp4")
 
-    try:
-        # Load the audio file
-        with sr.AudioFile(audio_file_path) as source:
-            audio_data = recognizer.record(source)
+# Extract the audio from the video
+audio_file = video.audio
+audio_file.write_audiofile("wave.wav")
 
-        # Attempt transcription using the Google Web Speech API
-        try:
-            text = recognizer.recognize_google(audio_data, show_all=True)
-            if not text:
-                # If Google API result is empty, try the Sphinx engine
-                text = recognizer.recognize_sphinx(audio_data)
+# Initialize recognizer
+r = sr.Recognizer()
 
-        except sr.UnknownValueError:
-            # If both engines fail to recognize, print an error message
-            print("Both recognition engines could not understand audio")
-            return
+# Load the audio file
+with sr.AudioFile("wave.wav") as source:
+	data = r.record(source)
 
-        # Create a text file name by replacing the audio file extension with '.txt'
-        text_file_name = os.path.splitext(os.path.basename(audio_file_path))[0] + '.txt'
+# Convert speech to text
+text = r.recognize_google(data)
 
-        # Get the directory where the audio file is located
-        output_folder = os.path.dirname(audio_file_path)
-
-        # Create the full path for the text file
-        text_file_path = os.path.join(output_folder, text_file_name)
-
-        # Save the transcription to the text file
-        with open(text_file_path, 'w') as text_file:
-            text_file.write(text)
-
-        print(f"Transcription saved to: {text_file_path}")
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    audio_file_path = "D://techolution//video_to_audio//video_capture//output_audio.wav"  # Replace with the path to your audio file
-    transcribe_audio(audio_file_path)
+# Print the text
+print("\nThe resultant text from video is: \n")
+print(text)
+with open('readme.txt', 'a') as f:
+        f.write(''.join(text))
